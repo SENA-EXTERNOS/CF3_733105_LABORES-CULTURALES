066850e4c455a9ab4b29a99d907c2738a3921742
Leonardo-Pico <63673074+Leonardo-Pico@users.noreply.github.com>
2023-03-28 16:29:34 -0500

    Video

--- src/views/Tema2.vue
+++ src/views/Tema2.vue
@@ -518,7 +518,7 @@
 
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/2L91WMqw96A" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/-tImQF2ynTk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
       p Frente al uso de fertilizantes, la norma propende por la protección de las actividades agropecuarias y agrícolas a fin de evitar posibles enfermedades, o la posible expansión de contagios.
 
